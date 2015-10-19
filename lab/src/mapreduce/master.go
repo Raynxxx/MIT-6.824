c8f6caa26448aaaf77f83d05eae89fc60ee6f1ee
Raynxxx <rayn1027@outlook.com>
2015-10-19 17:06:40 +0800

    pass Lab1

--- lab/src/mapreduce/master.go
+++ lab/src/mapreduce/master.go
@@ -3,12 +3,10 @@ package mapreduce
 import "container/list"
 import "fmt"
 
-
 type WorkerInfo struct {
 	address string
 	// You can add definitions here.
 }
-
 
 // Clean up all workers by sending a Shutdown RPC to each one of them Collect
 // the number of jobs each work has performed.
@@ -30,5 +28,58 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+	mapChan := make(chan int, mr.nMap)
+	reduceChan := make(chan int, mr.nReduce)
+
+	handOutJob := func(worker string, operation JobType, index int) bool {
+		jobArgs := &DoJobArgs{mr.file, operation, index, 0}
+		reply := &DoJobReply{}
+		switch operation {
+		case Map:
+			jobArgs.NumOtherPhase = mr.nReduce
+		case Reduce:
+			jobArgs.NumOtherPhase = mr.nMap
+		default:
+			fmt.Printf("handOutJob: unknown Operation\n")
+		}
+		return call(worker, "Worker.DoJob", jobArgs, reply)
+	}
+
+	scheduler := func(okChan chan int, operation JobType, index int) {
+		for {
+			var worker string
+			var isOk bool = false
+			select {
+			case worker = <-mr.idleChannel:
+				isOk = handOutJob(worker, operation, index)
+			case worker = <-mr.registerChannel:
+				isOk = handOutJob(worker, operation, index)
+			}
+			if isOk {
+				okChan <- index
+				mr.idleChannel <- worker
+				return
+			}
+		}
+	}
+
+	for i := 0; i < mr.nMap; i++ {
+		go scheduler(mapChan, Map, i)
+	}
+
+	for i := 0; i < mr.nMap; i++ {
+		id := <-mapChan
+		fmt.Printf("Map %d has done\n", id)
+	}
+
+	for i := 0; i < mr.nReduce; i++ {
+		go scheduler(reduceChan, Reduce, i)
+	}
+
+	for i := 0; i < mr.nReduce; i++ {
+		id := <-reduceChan
+		fmt.Printf("Reduce %d has done\n", id)
+	}
+
 	return mr.KillWorkers()
 }
